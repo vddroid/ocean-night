d5da51e1e983e9303fc12bc477438b808faa582a
vddroid <lilhonkie@gmail.com>
2020-08-31 10:29:22 +1000

    1

---
+++ contact us.js
@@ -0,0 +1,29 @@
+var L;
+
+window.onload = function () {
+   L.mapquest.key = "YOUR-API-KEY-HERE";
+
+   // 'map' refers to a <div> element with the ID map
+   var map = L.mapquest.map("map", {
+      center: [53.480759, -2.242631],
+      // 1. change 'map' to 'hybrid', try other type of map
+      layers: L.mapquest.tileLayer("map"),
+      zoom: 16,
+   });
+
+   // 2. Add control
+   map.addControl(L.mapquest.control());
+
+   // 3. Add icon
+   L.marker([53.480759, -2.242631], {
+      icon: L.mapquest.icons.marker({
+         primaryColor: "#22407F",
+         secondaryColor: "#3B5998",
+         shadow: true,
+         size: "md",
+         symbol: "A",
+      }),
+   })
+      .bindPopup("This is Manchester!")
+      .addTo(map);
+};
